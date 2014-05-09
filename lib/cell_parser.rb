ce33826ce87386c85303eac8adccf00153e6543e
Ilya Paripsa <iparipsa@thoughtworks.com>
2014-05-09 14:27:09 +1000

    implement rendering string from a list of cells

--- lib/cell_parser.rb
+++ lib/cell_parser.rb
@@ -1,3 +1,5 @@
+require 'cell'
+
 module CellParser
 
   class << self
@@ -6,6 +8,24 @@ module CellParser
       lines = grid_string.split("\n")
       lines.map { |line| parse_line(line) }.flatten
     end
+
+    def render(width = nil, cells)
+      width ||= cells.size
+      string = ''
+
+      cells.each_with_index do |cell, index|
+        string += "\n" if index != 0 && index % width == 0
+        string += render_single(cell)
+      end
+
+      string
+    end
+
+    def render_single cell
+      cell.alive? ? '*' : '-'
+    end
+
+    private
 
     def parse_line line
       cells = line.split(//)
--- spec/cell_parser_spec.rb
+++ spec/cell_parser_spec.rb
@@ -1,15 +1,62 @@
 require 'cell_parser'
 
-describe "GameParser" do
+describe "CellParser" do
 
-  describe "parsing a single cell" do
+  describe "string to cells" do
 
-    it "parses an dead cell" do
-      expect(CellParser.parse('-').first.alive?).to be_false
+    describe "parsing a single cell" do
+
+      it "parses an dead cell" do
+        expect(CellParser.parse('-').first.alive?).to be_false
+      end
+
+      it "parses a live cell" do
+        expect(CellParser.parse('*').first.alive?).to be_true
+      end
+
     end
 
-    it "parses a live cell" do
-      expect(CellParser.parse('*').first.alive?).to be_true
+    describe "parsing multiple cells on a single line" do
+
+      it "parses 3 cells with a mix of dead and alive ones" do
+        cells = CellParser.parse('-*-')
+        expect(cells.map(&:alive?)).to match_array([false, true, false])
+      end
+
+    end
+
+    describe "parsing multiple cells on a multiple line" do
+
+      it "parses 2 lines of cells" do
+        cells = CellParser.parse("-*-\n---")
+        expect(cells.map(&:alive?)).to match_array([false, true, false, false, false, false])
+      end
+
+    end
+
+  end
+
+  describe "cells to string" do
+
+    it "renders one dead cell" do
+      expect(CellParser.render_single(Cell.new(false))).to eq('-')
+    end
+
+    it "renders one live cells" do
+      expect(CellParser.render_single(Cell.new(true))).to eq('*')
+    end
+
+    it "renders a list of mixed cells" do
+      expect(CellParser.render([Cell.new(true), Cell.new(false)])).to eq('*-')
+    end
+
+    describe "rendering multiple lines" do
+
+      it "renders a list of mixed sells given width" do
+        result = CellParser.render(2, [Cell.new(true), Cell.new(false), Cell.new(true), Cell.new(false)])
+        expect(result).to eq("*-\n*-")
+      end
+
     end
 
   end
