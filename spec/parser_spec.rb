2781655dc9228dfe273f2036c1b32d6d76b9f735
Ilya Paripsa <iparipsa@thoughtworks.com>
2014-05-10 16:15:52 +1000

    cleanup

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,38 +1,34 @@
 require 'matrix'
 require 'parser'
 
-describe "Parser" do
+describe Parser do
 
-  describe "string to cells" do
+  describe "parsing a single cell" do
 
-    describe "parsing a single cell" do
-
-      it "parses an dead cell" do
-        expect(Parser.parse('-').first.alive?).to be_false
-      end
-
-      it "parses a live cell" do
-        expect(Parser.parse('*').first.alive?).to be_true
-      end
-
+    it "parses an dead cell" do
+      expect(Parser.parse('-').first.alive?).to be_false
     end
 
-    describe "parsing multiple cells on a single line" do
-
-      it "parses 3 cells with a mix of dead and alive ones" do
-        cells = Parser.parse('-*-')
-        expect(cells.row(0).to_a.map(&:alive?)).to match_array([false, true, false])
-      end
-
+    it "parses a live cell" do
+      expect(Parser.parse('*').first.alive?).to be_true
     end
 
-    describe "parsing multiple cells on a multiple line" do
+  end
 
-      it "parses 2 lines of cells" do
-        cells = Parser.parse("-*-\n---")
-        expect(cells.to_a.flatten.map(&:alive?)).to match_array([false, true, false, false, false, false])
-      end
+  describe "parsing multiple cells on a single line" do
 
+    it "parses 3 cells with a mix of dead and alive ones" do
+      cells = Parser.parse('-*-')
+      expect(cells.row(0).to_a.map(&:alive?)).to match_array([false, true, false])
+    end
+
+  end
+
+  describe "parsing multiple cells on a multiple line" do
+
+    it "parses 2 lines of cells" do
+      cells = Parser.parse("-*-\n---")
+      expect(cells.to_a.flatten.map(&:alive?)).to match_array([false, true, false, false, false, false])
     end
 
   end
